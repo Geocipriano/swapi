cdb6cd0ab2e87f0e7b62c97649035b4885390e00
Geovany Cipriano <geocipriano28@outlook.com>
2021-03-15 13:35:57 -0300

    Criando Estrutura

---
+++ src/components/navbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="navbar">
+      <div class="logo"><img src="../assets/logo.png" alt="logo"></div>
+      <div class="right"></div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Navbar'
+}
+</script>
+
+<style>
+
+.logo {
+    width: 50px;
+    height: 50px;
+}
+.logo img {
+    width: 100%;
+}
+</style>
