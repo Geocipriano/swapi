2c957d28c72b43bbd6553a08a709fc34e4f5ec34
Geovany Cipriano <geocipriano28@outlook.com>
2021-03-15 16:57:43 -0300

    Menu

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -3,14 +3,27 @@
         <nav>
             <a href="/" class="container"><img src="../assets/logo.png" alt="logo" id="logo"></a>
             <img src="../assets/menu.png" alt="menu-button" id="menu-button" class="container">
-            <div id="menu-overlay"></div>
+            <div id="menu-overlay" v-if="menuActive"></div>
+            <div id="menu-itens" :class="{active:menuActive}">
+                <a href="/"><img src="../assets/logo.png" alt="logo" id="menu-logo"></a>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/sobre">Sobre</a></li>
+                    <li><a href="/filmes">Filmes</a></li>
+                </ul>
+            </div>
         </nav>
     </header>
 </template>
 
 <script>
 export default {
-    name: 'Navbar'
+    name: 'Navbar',
+    data(){
+        return{
+            menuActive: true
+        }
+    }
 }
 </script>
 
@@ -36,6 +49,39 @@ nav {
     position: fixed;
     top: 0;
     left: 0;
+    width: 40%;
+    height: 100vh;
+    background-color: #0000;
+    opacity: 0.8;
+}
+#menu-logo {
+    width: 110px;
+}
+#menu-itens {
+    position: fixed;
+    top: 0;
+    right: 0;
+    background-color: var(--color-black);
+    width: 60%;
+    height: 100vh;
+    display: none;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+}
+#menu-itens .active {
+    display: flex;
+}
+#menu-itens ul li {
+    margin-bottom: 20px;
 }
 
+#menu-itens ul {
+    text-align: center;
+}
+
+#menu-itens ul li a {
+    font-size: 20px;
+    color: var(--color-yellow);
+}
 </style>
